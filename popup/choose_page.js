35a8d5370c9c2991c956ca13a7079b93386cab36
Vedant Roy <vroy101@gmail.com>
2018-04-07 17:30:41 -0400

    Convert to all token mechanism

--- popup/choose_page.js
+++ popup/choose_page.js
@@ -1,9 +1,24 @@
+/* 
+TODO.txt
+-Implement recognition of commas, periods, and new lines. (There are three patterns: 
+																					[SM_g]word[SM_h]
+																					[SM_g]word[SM_h]<punctuation_mark> (e.g - [SM_g]word[SM_h].)
+																					[SM_g]word[SM_h]<punctuation_mark>[SM_1])
+
+																					where [SM_1] denotes a "newline"
+
+
+Figure out how to do elegant async calls with returnSummary()
+*/
+
+
 document.addEventListener("click", e => {
 
     const buttonID = getButtonID(e.target);
 
     if (!(buttonID === "no-button-clicked") && !(buttonID == "button-clicked-but-no-id")) {
-        returnSummary(buttonID);
+    	//call returnSummary function and return its value
+    	returnSummary(buttonID);
     } else {
         console.log(buttonID);
     }
@@ -16,18 +31,40 @@ function returnSummary(summaryLength) {
 
             const activeTabURL = tabs[0].url;
             const nonTokenURL = 'https://smmry.com/' + activeTabURL + '#&SM_LENGTH=' + summaryLength.toString();
-            console.log("URL: " + nonTokenURL)
+            //console.log("URL: " + nonTokenURL)
 
             fetch(nonTokenURL)
                 .then((nonTokenResponse) => nonTokenResponse.text())
                 .then(nonTokenResponseText => {
-                    console.log("Fetching Token")
+
                     const summaryToken = nonTokenResponseText.match(/TOKEN=(.*?)&/);
                     const summaryTokenCompiledURL = "https://smmry.com/sm_portal.php?&SM_TOKEN=" + summaryToken[1] + "&SM_POST_SAVE=0&SM_REDUCTION=-1&SM_CHARACTER=-1&SM_LENGTH=" + summaryLength.toString() + "&SM_URL=" + activeTabURL;
-                    console.log("Value of summaryTokenURL: " + summaryTokenCompiledURL);
+
+                    browser.tabs.create({ url: summaryTokenCompiledURL });
                     fetch(summaryTokenCompiledURL).then((tokenSummaryResponse) => tokenSummaryResponse.text())
                         .then(tokenResponseText => {
-                            console.log(tokenResponseText);
+                        	//Include conditions for "Summary too short/Summary Unrecognized"
+                        	const summaryRegexPattern = /\[SM_g](.*?)\[SM_h]/g;
+	                        var match;
+
+	                        var summary = "";
+
+                        	do {
+                        		match = summaryRegexPattern.exec(tokenResponseText);
+                        		if(match) {
+                        			summary += match[1] + " ";
+                        		}
+                        	} while (match);
+
+                        	if(summary != "") {
+                        		console.log("Summary: " + summary);
+                        	} else if (tokenResponseText.includes('THE PAGE IS IN AN UNRECOGNISABLE FORMAT')) {
+                        		console.log("CHROME-WEB-EXTENSION-ERROR-PAGE-UNRECOGNISABLE");
+                        	} else if (tokenResponseText.includes('SOURCE IS TOO SHORT')) {
+                        		console.log("CHROME-WEB-EXTENSION-ERROR-PAGE-TOO-SHORT");
+                        	} else {
+                        		console.log("CHROME-WEB-EXTENSION-ERROR-PAGE-TOO-SHORT");
+                        	}
                         });
                 })
         });
