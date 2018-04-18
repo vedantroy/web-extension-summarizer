7605f885658e8d2cf449c4e59d0d9fc1a3b31d35
Vedant Roy <vroy101@gmail.com>
2018-04-18 15:14:35 -0400

    Added New Line Functionality

--- inject.js
+++ inject.js
@@ -15,34 +15,34 @@
      */
     browser.runtime.onMessage.addListener((message) => {
 
-		var iFrame = document.createElement("iFrame");
-		iFrame.id = "contentFrame";
-		iFrame.style.cssText = "width: 100%; height: 100%; border: none;";
-		iFrame.src = browser.extension.getURL("inject.html");
+        var iFrame = document.createElement("iFrame");
+        iFrame.id = "contentFrame";
+        iFrame.style.cssText = "width: 100%; height: 100%; border: none;";
+        iFrame.src = browser.extension.getURL("inject.html");
 
-		var boxDiv = document.createElement("div");
-		boxDiv.style.cssText = "background: white; box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 9px 8px; height: 100%; left: calc(100% - 390px); position: fixed; top: 0px; width: 390px; z-index: 1;"
+        var boxDiv = document.createElement("div");
+        boxDiv.style.cssText = "background: white; box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 9px 8px; height: 100%; left: calc(100% - 390px); position: fixed; top: 0px; width: 390px; z-index: 1;"
 
-		var zeroDiv = document.createElement("div");
-		zeroDiv.style.cssText = "position: fixed; width: 0px; height: 0px; top: 0px; left: 0px; z-index: 2147483647;";
+        var zeroDiv = document.createElement("div");
+        zeroDiv.style.cssText = "position: fixed; width: 0px; height: 0px; top: 0px; left: 0px; z-index: 2147483647;";
 
-		var outerDiv = document.createElement("div");
+        var outerDiv = document.createElement("div");
 
-		boxDiv.appendChild(iFrame);
-		zeroDiv.appendChild(boxDiv);
-		outerDiv.appendChild(zeroDiv);
-		document.body.appendChild(outerDiv);
+        boxDiv.appendChild(iFrame);
+        zeroDiv.appendChild(boxDiv);
+        outerDiv.appendChild(zeroDiv);
+        document.body.appendChild(outerDiv);
 
         returnSummary(message.summaryLength, message.targetURL).then(summary => {
 
-        	summaryBox = document.getElementById("contentFrame").contentWindow.document.getElementById("summary");
+            summaryBox = document.getElementById("contentFrame").contentWindow.document.getElementById("summary");
 
-        	if(summary.status == "no-error") {
-        		summaryBox.innerHTML = summary.summary;
+            if (summary.status == "no-error") {
+                summaryBox.innerHTML = summary.summary;
 
-        	} else {
-        		summaryBox.innerHTML = summary.status;
-        	}
+            } else {
+                summaryBox.innerHTML = summary.status;
+            }
         });
     });
 
@@ -59,19 +59,19 @@
                     const summaryTokenCompiledURL = 'https://smmry.com/sm_portal.php?&SM_TOKEN=' + summaryToken[1] + '&SM_POST_SAVE=0&SM_REDUCTION=-1&SM_CHARACTER=-1&SM_LENGTH=' + summaryLength.toString() + '&SM_URL=' + targetURL;
 
                     //Temporary
+                    //TODO - find more elegant way of adding in double new lines.
                     console.log(summaryTokenCompiledURL);
 
                     fetch(summaryTokenCompiledURL).then((tokenSummaryResponse) => tokenSummaryResponse.text())
                         .then(tokenResponseText_initial => {
 
+                        	//tokenResponseText_initial = "[SM_g]This[SM_h][SM_g]is[SM_h][SM_g]a[SM_h][SM_g]sentence[SM_h].[SM_l][SM_g]Here[SM_h][SM_g]is[SM_h][SM_g]another[SM_h][SM_g]sentence[SM_h].[SM_1]"
+
                             const tokenResponseText_fixedPeriodsCommas = tokenResponseText_initial.replace(/(\[SM_g].*?)(\[SM_h])([.,])/g, "$1$3$2");
 
-                            console.log("Fixed for Periods/Commas: " + tokenResponseText_fixedPeriodsCommas);
+                            const newLineIndicator = "rstudios-web-extension-summarizer-double-new-line-uniqueID"
 
-                            const tokenResponseText_fixedNewLines = tokenResponseText_fixedPeriodsCommas.replace(/(\[SM_g].*?)(\[SM_h]\[SM_I])/g, "$1\n");
-
-                            //This doesn't work yet
-                            console.log("Fixed For New Lines:" + tokenResponseText_fixedNewLines);
+                            const tokenResponseText_fixedNewLines = tokenResponseText_fixedPeriodsCommas.replace(/(\[SM_g].*?)(\[SM_h]\[SM_l])/g, "$1" + newLineIndicator + "$2");
 
                             const wordCompilationRegex = /\[SM_g](.*?)\[SM_h]/g;
                             var wordRegexResponse;
@@ -81,7 +81,11 @@
                             do {
                                 wordRegexResponse = wordCompilationRegex.exec(tokenResponseText_fixedNewLines);
                                 if (wordRegexResponse) {
-                                    summary += wordRegexResponse[1] + " ";
+                                    if (wordRegexResponse[1].includes(newLineIndicator)) {
+                                        summary += wordRegexResponse[1].replace(newLineIndicator, "") + "\n\n";
+                                    } else {
+                                        summary += wordRegexResponse[1] + " ";
+                                    }
                                 }
                             } while (wordRegexResponse);
 
