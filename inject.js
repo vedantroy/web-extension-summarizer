1c1124623b3d38616d96f6995c514a4eab412a3c
Vedant Roy <vroy101@gmail.com>
2018-04-21 16:26:26 -0400

    Copy Button Works

--- inject.js
+++ inject.js
@@ -48,21 +48,31 @@
 
                 copyButton.addEventListener("click", () => {
                     console.log("Clicking Copy Button");
-                    var sBox = iFrame.contentWindow.document.getElementById("summary");
-                    console.log(sBox.innerHTML);
-                    var range = document.createRange();
-                    var selection = window.getSelection();
-                    range.selectNodeContents(sBox);
-                    selection.removeAllRanges();
-                    selection.addRange(range);
-
+                    var summaryContainer = iFrame.contentWindow.document.getElementById("summary");
+                    var textarea_temp = document.createElement("textarea");
+                    textarea_temp.style.position = 'fixed';
+                    textarea_temp.style.top = 0;
+                    textarea_temp.style.left = 0;
+                    textarea_temp.style.width = '2em';
+                    textarea_temp.style.height = '2em';
+                    textarea_temp.style.padding = 0;
+                    textarea_temp.style.border = 'none';
+                    textarea_temp.style.outline = 'none';
+                    textarea_temp.style.boxShadow = 'none';
+                    textarea_temp.style.background = 'transparent';
+                    textarea_temp.value = summaryContainer.innerHTML;
+                    document.body.appendChild(textarea_temp);
+                    textarea_temp.focus();
+                    textarea_temp.select();
                     try {
                         var successful = document.execCommand('copy');
                         var msg = successful ? 'successful' : 'unsuccessful';
                         console.log('Copying text command was ' + msg);
-                    } catch (err) {
-                        console.log('Oops, unable to copy');
+                    } catch (error) {
+                        console.log('Error: ' + error);
                     }
+
+                    document.body.removeChild(textarea_temp);
 
                 });
 
